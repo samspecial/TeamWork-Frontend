b36a450b13d8101021cda045f7c9973a5197da9b
Osinloye Samuel Adebayo <psalmueloye@gmail.com>
2019-12-07 04:04:50 +0100

    Updates

---
+++ precache-manifest.5b9a79107d3f8cfaaf59a24d440abdb5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43194a7004213415106107fd118db3de",
+    "url": "/teamwork-frontend/index.html"
+  },
+  {
+    "revision": "67ec9ddf52a9d3984934",
+    "url": "/teamwork-frontend/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "8b6cc89c21c9a5b672e4",
+    "url": "/teamwork-frontend/static/css/main.0eebb37e.chunk.css"
+  },
+  {
+    "revision": "67ec9ddf52a9d3984934",
+    "url": "/teamwork-frontend/static/js/2.0dd6a644.chunk.js"
+  },
+  {
+    "revision": "8b6cc89c21c9a5b672e4",
+    "url": "/teamwork-frontend/static/js/main.bde3bb11.chunk.js"
+  },
+  {
+    "revision": "c72af4ddc98ec8359cab",
+    "url": "/teamwork-frontend/static/js/runtime-main.b288330e.js"
+  },
+  {
+    "revision": "9315804b93ebdc18a751eb251b2e851c",
+    "url": "/teamwork-frontend/static/media/colabo.9315804b.png"
+  },
+  {
+    "revision": "018a247448aeb2cb8be8c4d38fefd059",
+    "url": "/teamwork-frontend/static/media/communicate.018a2474.png"
+  },
+  {
+    "revision": "9cd3b33b00d0785cc358a93f8ae790c2",
+    "url": "/teamwork-frontend/static/media/network.9cd3b33b.png"
+  },
+  {
+    "revision": "2b630837a48a05130e2009f37c03269e",
+    "url": "/teamwork-frontend/static/media/social.2b630837.png"
+  },
+  {
+    "revision": "de0ecb8621affc9ddf98da1f6c2078c2",
+    "url": "/teamwork-frontend/static/media/team.de0ecb86.png"
+  },
+  {
+    "revision": "9e90d7797e50aa490242f91d273f6e8e",
+    "url": "/teamwork-frontend/static/media/teamwork.9e90d779.png"
+  }
+]);
